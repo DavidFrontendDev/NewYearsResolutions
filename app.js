644f71de9cb7c5df76b7c20afbf041f81f34d992
DavidFrontendDev <daviddevbackend@gmail.com>
2025-01-29 15:54:37 +0100

    Todo funcional me falta el buscador

--- app.js
+++ app.js
@@ -11,28 +11,80 @@ function Resolution(titulo, texto, completada) {
     );
   };
 }
+function eliminarResolution(titulo, divElemento) {
+  resolutions = resolutions.filter((res) => res.titulo !== titulo);
+  divElemento.remove();
+}
 
-function crearResolution() {
+function crearResolution(titulo, texto) {
+  const main = document.querySelector("main");
   let div = document.createElement("div");
-  div.className("card");
-
+  div.className = "card";
   let divTitulo = document.createElement("div");
-  divTitulo.className("card-title");
-
+  divTitulo.className = "card-title";
+  divTitulo.textContent = titulo;
   let divTexto = document.createElement("div");
-  divTexto.className("card-text");
-
+  divTexto.className = "card-text";
+  divTexto.textContent = texto;
   let divBotones = document.createElement("div");
-  divBotones.className("card-actions");
-
+  divBotones.className = "card-actions";
   let botonCompletar = document.createElement("button");
   botonCompletar.textContent = "Completar";
+  botonCompletar.addEventListener("click", () => {
+    let index = resolutions.findIndex(
+      (res) => res.titulo === divTitulo.textContent
+    );
+    if (index !== -1) {
+      // si lo encuentra
+      resolutions[index].completada = true;
+      divTitulo.innerHTML = `<del style="background-color: rgba(169, 169, 169, 0.5);">${resolutions[index].titulo} ✔</del>`;
+      divTexto.innerHTML = `<del style="background-color: rgba(169, 169, 169, 0.5);">${resolutions[index].texto} ✔</del>`;
+      botonCompletar.style.backgroundColor = "red";
+      botonCompletar.textContent = "Completado!";
+      botonEditar.style.display = "none";
+    }
+  });
+
   let botonEditar = document.createElement("button");
   botonEditar.id = "botonDos";
   botonEditar.textContent = "Editar";
+  botonEditar.addEventListener("click", () => {
+    let tituloOriginal = divTitulo.textContent; // Guardamos el titulo antes de cambiarlo
+    let nuevoTitulo = prompt(
+      "What's the new title of the resolution?",
+      tituloOriginal
+    );
+    let nuevoTexto = prompt(
+      "What's the new text of the resolution?",
+      divTexto.textContent
+    );
+    if (nuevoTitulo && nuevoTexto) {
+      divTitulo.textContent = nuevoTitulo;
+      divTexto.textContent = nuevoTexto;
+      let index = resolutions.findIndex((res) => res.titulo === tituloOriginal);
+      if (index !== -1) {
+        // si lo encuentra
+        resolutions[index].titulo = nuevoTitulo;
+        resolutions[index].texto = nuevoTexto;
+      }
+    }
+  });
+
   let botonEliminar = document.createElement("button");
   botonEliminar.id = "botonTres";
   botonEliminar.textContent = "Eliminar";
+  botonEliminar.addEventListener("click", () => {
+    div.remove();
+    eliminarResolution(titulo, div);
+  });
+
+  div.appendChild(divTitulo);
+  div.appendChild(divTexto);
+  divBotones.appendChild(botonCompletar);
+  divBotones.appendChild(botonEditar);
+  divBotones.appendChild(botonEliminar);
+  div.appendChild(divBotones);
+  main.appendChild(div);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -40,23 +92,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const openModalBtn = document.querySelector(".new-resolution-container i");
   const closeModalBtn = document.querySelector(".close");
   const botonMeterResolucion = document.querySelector("#meter-resolution");
-  const addResolutionBtn = document.getElementById("add-resolution");
-
   openModalBtn.addEventListener("click", () => {
     modal.style.display = "flex";
   });
-
   closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
   botonMeterResolucion.addEventListener("click", () => {
     modal.style.display = "none";
-    // logica
-  });
-
-  window.addEventListener("click", (event) => {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
+    const titulo = document.querySelector("#new-resolution");
+    let tituloResolucion = titulo.value;
+    const texto = document.querySelector("#new-text-resolution");
+    let textoRes = texto.value;
+    let resolucion = new Resolution(tituloResolucion, textoRes, false);
+    resolutions.push(resolucion);
+    crearResolution(tituloResolucion, textoRes);
+    titulo.value = "";
+    texto.value = "";
   });
 });
